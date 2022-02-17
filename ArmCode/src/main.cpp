51f97efc5a4e64b457f2b81ac7eee696559b5c39
Areeb-M <am0247@uah.edu>
2022-02-17 12:18:58 -0600

    Removed loop

--- ArmCode/src/main.cpp
+++ ArmCode/src/main.cpp
@@ -6,6 +6,7 @@ Stream* MySerial;
 // Linear Actuator Declarations
 Actuator Axis2(18, 19, 22, 23, 0, 17000, 317.5, 93.0, 390.0, 38.1/4000.0);
 
+
 void setup()
 {
     #pragma region Serial Configuration
@@ -57,16 +58,5 @@ void loop()
         Axis2.GetSpeed(), Axis2.GetDirection(), Axis2.GetStep(), Axis2.GetTarget(),
         Axis2.GetAngle(), Axis2.GetAngularRate());
         lastTime = millis();
-    }
-
-    if (!Axis2.IsActive())
-    {
-        toggle = !toggle;
-        
-        if (toggle)
-            Axis2.Extend(3000);
-        else
-            Axis2.Extend(-3000);
-
     }
 }
