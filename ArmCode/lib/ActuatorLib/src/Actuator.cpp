7640270aa08ceae1f8395fc10e82e2654a011915
Areeb-M <am0247@uah.edu>
2022-02-16 20:28:03 -0600

    Skeleton code for Actuator class

---
+++ ArmCode/lib/ActuatorLib/src/Actuator.cpp
@@ -0,0 +1,82 @@
+#include "Actuator.h"
+
+Actuator::Actuator(int pwmPin, int dirPin, int encoderPinA,
+        int encoderPinB, int lowerLimit, int upperLimit,
+        float baseLength, float sideALength, float sideBLength, 
+        float dxde)
+{
+        this->pwmPin = pwmPin;
+        this->dirPin = dirPin;
+        this->encoderPinA = encoderPinA;
+        this->encoderPinB = encoderPinB;
+        this->lowerLimit = lowerLimit;
+        this->upperLimit = upperLimit;
+        this->baseLength = baseLength;
+        this->sideALength = sideALength;
+        this->sideBLength = sideBLength;
+        this->dxde = dxde;
+}
+
+void Actuator::Initalize()
+{
+        // Initialize Control Pins
+        pinMode(pwmPin, OUTPUT);
+        pinMode(dirPin, OUTPUT);
+        SetSpeed(0);
+        SetDirection(EXTEND);
+
+        // Initialize Encoder
+        encoder = new Encoder(encoderPinA, encoderPinB);
+        encoder->readAndReset();
+
+        ResetBuffers();
+}
+
+void Actuator::ResetBuffers()
+{
+        // Clear buffers
+        encoderBuffer.clear();
+        timingBuffer.clear();
+
+        encoderBuffer.unshift(encoder->read());
+        timingBuffer.unshift(millis());
+}
+
+void Actuator::Update()
+{
+        // Check state
+        // If target mode, then 
+}
+
+void Actuator::Home(bool retract)
+{
+        // Set the right direction for homing
+        if (retract)
+                SetDirection(RETRACT);
+        else
+                SetDirection(EXTEND);
+        
+        // Log the start position, then power the actuator
+        int step = encoder->read();
+        SetSpeed(HOMING_SPEED);
+
+        // Check every 100ms whether the actuator has moved
+        // If not, break out of the loop
+        do
+        {
+                delay(100);
+                int nextStep = encoder->read();
+
+                if (nextStep == step)
+                        break;
+                else
+                        step = nextStep;
+        } while (1);
+        
+        // Power down actuator
+        SetSpeed(0);
+
+        // Reset encoder and buffers
+        encoder->readAndReset();
+        ResetBuffers();
+}
---
+++ ArmCode/lib/ActuatorLib/src/Actuator.h
@@ -0,0 +1,64 @@
+#include <CircularBuffer.h>
+#include <Encoder.h>
+
+#define EXTEND LOW
+#define RETRACT HIGH
+
+#define BUFFER_SIZE 50          // Size of the encoder buffer
+#define BUFFER_TIME_STEP 10     // How often to add to the buffer
+
+#define HOMING_SPEED 128        // Speed to use when homing actuators
+
+class Actuator
+{
+    private:
+        // Pins to drive the actuator
+        int pwmPin;
+        int dirPin;
+
+        // Pins to connect to the encoder
+        int encoderPinA;
+        int encoderPinB;
+        Encoder* encoder;
+
+        // Safety limits on the actuators
+        // Prevents over or under extension
+        int lowerLimit;
+        int upperLimit;
+
+        // Variables to calculate the arm angle
+        float dxde; // change in extension per encoder step
+        float baseLength; // Length of actuator
+        float sideALength;
+        float sideBLength;
+        float extension; // Calculated extension of the actuator
+
+        // Actuator Speed Tracking
+        CircularBuffer<int, BUFFER_SIZE> encoderBuffer;
+        CircularBuffer<int, BUFFER_SIZE> timingBuffer;
+
+        void ResetBuffers(); // Safely resets the buffers
+
+        // Helper Functions to Control the Actuator
+        void SetSpeed(short speed) { analogWrite(pwmPin, speed); }
+        void SetDirection(short direction) { digitalWrite(dirPin, direction); }
+
+        // Control Loop
+        void Update();
+
+    public:
+        Actuator(int pwmPin, int dirPin, int encoderPinA,
+        int encoderPinB, int lowerLimit, int upperLimit,
+        float baseLength, float sideALength, float sideBLength, 
+        float dxde);
+
+        void Initalize();
+
+        // Target Based Control
+        void Extend(int steps);
+        void Retract(int steps);
+
+        // This function homes the actuator
+        // This function is blocking
+        void Home(bool retract=true);
+};
